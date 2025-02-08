9bcb7e37f5a5cc3ab6c622deb8b0788e9842d1df
Favour Oladeji <oladejifavour100@gmail.com>
2025-02-08 10:01:28 +0100

    feat: update userfactor to match new schemat

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'is_admin' => false,
+            'phone_number' => fake()->phoneNumber(),
+            'role' => "user",
+            'address' => fake()->address,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
